74e84adbb2163e71e07132b12939d40b14ddd62e
calvinK587 <marshmallo230@gmail.com>
2020-02-20 06:11:13 +0000

    Added our strategy to team 12.

--- team12.py
+++ team12.py
@@ -10,6 +10,8 @@ team_name = 'Sasuke' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
+number_of_betrays = []
+
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -17,14 +19,16 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
+    iterated_number = 0
+    for b in their_history[-10]:
+      if b == "b":
+        iterated_number += 1
+        number_of_betrays.append(iterated_number)
+    if len(number_of_betrays) < 1:
+      return 'c'
+    else:
+      for betray in number_of_betrays:
+        return 'b'
+      if 'b' not in their_history[-2:]:
+        return 'c'
+        number_of_betrays = []
