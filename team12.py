df9c3f812ffdc969ddc471be0d8c13456b38eb5f
calvinK587 <marshmallo230@gmail.com>
2020-02-20 06:12:50 +0000

    Added our team strategy.

--- team12.py
+++ team12.py
@@ -7,28 +7,35 @@
 ####
 
 team_name = 'Sasuke' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'if this wins, follow @an9sh on instagram'
+strategy_description = 'Counts the number of times the other person has betrayed and continues to betray that many times. Starts with collude.'
     
-number_of_betrays = []
-
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
-    Make my move.
+    Make my move. In order to make a move, this strategy looks at past data, and makes a variable which counts the number of times they have betrayed in the past. Then, the algorithmn betrays the same number of time the other competitior has betrayed. The number of betrays queued will reset if the opponent colludes twice in a row.
+
+    Math:
+    betray = n
+    betray to the nth multiple
+    
     Returns 'c' or 'b'. 
     '''
+    number_of_betrays = 0
     iterated_number = 0
-    for b in their_history[-10]:
+    for b in their_history:
       if b == "b":
         iterated_number += 1
-        number_of_betrays.append(iterated_number)
-    if len(number_of_betrays) < 1:
+        number_of_betrays += 1
+    if number_of_betrays < 1:
       return 'c'
     else:
-      for betray in number_of_betrays:
+      while number_of_betrays > 0:
         return 'b'
+        number_of_betrays -= 1
+         
       if 'b' not in their_history[-2:]:
         return 'c'
-        number_of_betrays = []
+        number_of_betrays = 0
+
